1e7575193311bd1c891ea69c28fb1361a4432b6e
apollinaire.desbiens <apollinaire.desbiens@gmail.com>
2016-11-04 17:03:09 +0100

    test unitaire pour initialiser la base

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into Produit (id, name, variete, prix, provenance, dateCueillette) values (0, 'Pomme', 'PinkLady', 15, 'La ferme A Dédé', '16/09/2016'), (1, 'Pomme', 'Golden', 10, 'La ferme A Dédé', '10/09/2016') ,(2, 'Raisin', 'Centennial', 62, 'La ferme A Dédé', '20/09/2016'); 
-insert into Categorie(id, name, produits) values (0,'fruit',0), (1,'fruit',1), (2,'fruit',2);
+insert into Produit (id, name, Categorie_id, variete, prix, provenance, dateCueillette) values (0, 'Pomme', 0, 'PinkLady', 15, 'La ferme A Dédé', '16/09/2016'), (1, 'Pomme', 0, 'Golden', 10, 'La ferme A Dédé', '10/09/2016') ,(2, 'Raisin', 0, 'Centennial', 62, 'La ferme A Dédé', '20/09/2016'); 
+insert into Categorie(id, name) values (0,'fruit'), (1,'legume'), (2,'cremerie');
