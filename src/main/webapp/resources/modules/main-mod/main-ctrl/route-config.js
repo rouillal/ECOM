539f6623d6006d2b3f4e6dc3cc3b20a0dda9e1f6
Lorrie Rouillaux <lorrie.rouillaux@gmail.com>
2016-11-11 21:14:47 +0100

    bandeau menu avec images, redimensionnement et interactif

--- src/main/webapp/resources/modules/main-mod/main-ctrl/route-config.js
+++ src/main/webapp/resources/modules/main-mod/main-ctrl/route-config.js
@@ -1,36 +1,42 @@
 eComBioApp
-		.config([
-				'$routeProvider',
-				'$locationProvider',
-				function($routeProvider, $locationProvider) {
-					$routeProvider
-							.when(
-									'/catalog',
-									{
-										templateUrl : 'resources/modules/product-catalog-mod/product-catalog.html',
-										controller : 'ProductCatalogCtrl'
-									})
-							.when(
-									'/recipe',
-									{
-										templateUrl : 'resources/modules/recipe-mod/recipe.html',
-										controller : 'RecipeCtrl'
-									})
-							.when(
-									'/basket',
-									{
-										templateUrl : 'resources/modules/basket-mod/basket.html',
-										controller : 'BasketCtrl'
-									})
-							.when(
-									'/connect',
-									{
-										templateUrl : 'resources/modules/connect-mod/connect.html',
-										controller : 'ConnectCtrl'
-									}).otherwise({
-								redirectTo : '/catalog'
-							});
+.config([
+         '$routeProvider',
+         '$locationProvider',
+         function($routeProvider, $locationProvider) {
+        	 $routeProvider
+        	 .when(
+        			 '/catalog',
+        			 {
+        				 templateUrl : 'resources/modules/product-catalog-mod/product-catalog.html',
+        				 controller : 'ProductCatalogCtrl'
+        			 })
+        			 .when(
+        					 '/recipe',
+        					 {
+        						 templateUrl : 'resources/modules/recipe-mod/recipe.html',
+        						 controller : 'RecipeCtrl'
+        					 })
+        					 .when(
+        							 '/basket',
+        							 {
+        								 templateUrl : 'resources/modules/basket-mod/basket.html',
+        								 controller : 'BasketCtrl'
+        							 })
+        							 .when(
+        									 '/signin',
+        									 {
+        										 templateUrl : 'resources/modules/signin-mod/signin.html',
+        										 controller : 'SigninCtrl'
+        									 })
+        									 .when(
+        											 '/connect',
+        											 {
+        												 templateUrl : 'resources/modules/connect-mod/connect.html',
+        												 controller : 'ConnectCtrl'
+        											 }).otherwise({
+        												 redirectTo : '/catalog'
+        											 });
 
-					$locationProvider.html5Mode(false);
+        	 $locationProvider.html5Mode(false);
 
-				} ]);
+         } ]);
---
+++ src/main/webapp/resources/modules/signin-mod/ctrl/signin-ctrl.js
@@ -0,0 +1,3 @@
+eComBioApp.controller('SigninCtrl', [ '$scope','$window', function($scope,$window) {
+	$scope.ao="A faire .";
+} ]);
