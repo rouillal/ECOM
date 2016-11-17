f250a4541445c3f20e2c6fae4b59eedbbe976632
apollinaire.desbiens <apollinaire.desbiens@gmail.com>
2016-11-17 12:24:14 +0100

    re post du panier

--- src/main/java/fr/ecombio/rest/PanierResourceRESTService.java
+++ src/main/java/fr/ecombio/rest/PanierResourceRESTService.java
@@ -9,8 +9,10 @@ import org.jboss.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import fr.ecombio.data.PanierRepository;
@@ -35,6 +37,17 @@ public class PanierResourceRESTService {
 		
 		Panier panier = new Panier(commande);
         return repository.AjoutPanier(panier);
+	}
+	
+	@PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public void createUpdatePanier(@QueryParam("id") Long id, Collection<Article> commande) {
+		
+		//Collection<Article> articles = new Collection<Article>();
+		
+		Panier panier = repository.findById(id);
+		panier.setArticles(commande);
+		repository.updatePanier(panier);
 	}
 	
 }
