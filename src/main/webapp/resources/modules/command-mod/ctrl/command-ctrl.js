89bf8c71f32e78a056fbd10de734840865d38442
fredimage2 <fderue@gmail.com>
2016-11-17 15:17:22 +0100

    Info Commande suite

--- src/main/webapp/resources/modules/command-mod/ctrl/command-ctrl.js
+++ src/main/webapp/resources/modules/command-mod/ctrl/command-ctrl.js
@@ -1,6 +1,20 @@
-eComBioApp.controller('CommandCtrl', [ '$scope', '$location','commandSvc',
-		function($scope, $location,commandSvc) {
+eComBioApp.controller('CommandCtrl', [ '$scope', '$location','$window','commandSvc',
+		function($scope, $location,$window,commandSvc) {
 	$scope.commandInfo = commandSvc.getCommandInfo();
-	$scope.aa = $scope.commandInfo;
+	$scope.commandPaieInfo = commandSvc.getCommandPaieInfo();
+	$scope.payerInfoVoir = false;
+	$scope.listeHoraires=[];
+	for (i = 0; i < 14; i++) {
+	    var horaireTmp = new Object();
+	    horaireTmp['id'] = i;
+	    var h1=i+6;
+	    var h2=i+7;
+	    horaireTmp['libelle'] = ''+h1+':00 - '+h2+':00';
+	    $scope.listeHoraires.push(horaireTmp);
+	}
+	$scope.payerInfo = function() {
+		$scope.payerInfoVoir = true;
+	}
+	
 	//angular.toJson(listePanier);
 } ]);
