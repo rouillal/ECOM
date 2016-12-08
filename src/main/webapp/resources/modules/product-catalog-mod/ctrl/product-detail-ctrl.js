69f3cf73f42b0b4d06e53a93363c6327da221246
Lorrie Rouillaux <lorrie.rouillaux@gmail.com>
2016-12-08 14:48:11 +0100

    bricolage

--- src/main/webapp/resources/modules/product-catalog-mod/ctrl/product-detail-ctrl.js
+++ src/main/webapp/resources/modules/product-catalog-mod/ctrl/product-detail-ctrl.js
@@ -46,5 +46,11 @@ eComBioApp.controller('ProductDetailCtrl', [
 				   setTimeout(function () {
 				    $('#tooltip').tooltip('hide');
 				   }, 1000);
-				})
+				});
+			
+			$(window).ready(function(){
+			    $("#myBtnDetailsPdtRecette").click(function(){
+			        $("#selectedProductDisplay").modal();
+			    });
+			});
 		} ]);
