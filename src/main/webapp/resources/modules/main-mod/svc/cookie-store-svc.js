10373c824365f4ea4f7ae9db966f91c1da4a75ba
fredimage2 <fderue@gmail.com>
2016-12-09 18:15:59 +0100

    Persistence client avec cookies

---
+++ src/main/webapp/resources/modules/main-mod/svc/cookie-store-svc.js
@@ -0,0 +1,39 @@
+eComBioApp.factory('cookieStoreSvc', [ '$rootScope','$window','$cookies',
+                                    function($rootScope,$window,$cookies) {
+	
+	var storeLocalString = function(name,info) {
+		//$window.alert("to : "+name+" value : "+info);
+		$cookies.put(name,info/*,{
+		    expires: 600,
+		    secure: true
+		  }*/);
+	}
+	
+	var getStoredLocalString = function(name) {
+		//$window.alert("from : "+name+" value : "+$cookies.get(name));
+		return $cookies.get(name);
+	}
+	
+	var storeLocalItem = function(name,info) {
+		var infoJson = angular.toJson(info);
+		//$window.alert("to : "+name+" value : "+infoJson);
+		$cookies.put(name,infoJson/*,{
+			    expires: 600,
+			    secure: true
+			  }*/);
+	}
+	
+	var getStoredLocalItem = function(name) {
+		//$window.alert("from : "+name+" value : "+$cookies.get(name));
+		var objetJson = $cookies.get(name);
+		var ret = angular.fromJson(objetJson);
+		return ret;
+	}
+
+	return {
+		storeLocalString : storeLocalString,
+		getStoredLocalString : getStoredLocalString,
+		storeLocalItem : storeLocalItem,
+		getStoredLocalItem : getStoredLocalItem
+	};
+} ]);
